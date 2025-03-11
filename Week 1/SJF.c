6157aa7609924da92cafd846b6bd4b0db77530a5
ayaanstha <ayaanshrestha.cs23@bmsce.ac.in>
2025-03-11 12:39:22 +0530

    Update SJF.c

--- Week 1/SJF.c
+++ Week 1/SJF.c
@@ -5,27 +5,27 @@ int main() {
     int bt[20], p[20], wt[20], tat[20];
     float wavg = 0, tatavg = 0;
 
-    // Taking input for number of processes
+
     printf("Enter the number of processes: ");
     scanf("%d", &n);
 
-    // Input burst times and initialize process numbers
+    
     for (i = 0; i < n; i++) {
         printf("Enter Burst Time for Process %d: ", i + 1);
         scanf("%d", &bt[i]);
-        p[i] = i + 1; // Storing process number
+        p[i] = i + 1; 
     }
 
-    // Sorting processes by burst time (SJF)
+    
     for (i = 0; i < n - 1; i++) {
         for (j = i + 1; j < n; j++) {
             if (bt[i] > bt[j]) {
-                // Swap burst times
+                
                 temp = bt[i];
                 bt[i] = bt[j];
                 bt[j] = temp;
 
-                // Swap process numbers
+                
                 temp = p[i];
                 p[i] = p[j];
                 p[j] = temp;
@@ -33,30 +33,30 @@ int main() {
         }
     }
 
-    // Initialize waiting time for the first process
+    
     wt[0] = 0;
-    tat[0] = bt[0]; // Turnaround time for the first process is its burst time
+    tat[0] = bt[0]; 
 
-    // Calculate waiting time and turnaround time for each process
+    
     for (i = 1; i < n; i++) {
-        wt[i] = wt[i - 1] + bt[i - 1]; // Waiting time is the sum of burst times of previous processes
-        tat[i] = wt[i] + bt[i]; // Turnaround time is waiting time + burst time
+        wt[i] = wt[i - 1] + bt[i - 1]; 
+        tat[i] = wt[i] + bt[i]; 
 
-        wavg += wt[i]; // Summing up the waiting times
-        tatavg += tat[i]; // Summing up the turnaround times
+        wavg += wt[i]; 
+        tatavg += tat[i]; 
     }
 
-    // Calculate averages
+    
     wavg /= n;
     tatavg /= n;
 
-    // Output the results
+  
     printf("\nPROCESS\tBURST TIME\tWAITING TIME\tTURNAROUND TIME\n");
     for (i = 0; i < n; i++) {
         printf("%d\t\t%d\t\t%d\t\t%d\n", p[i], bt[i], wt[i], tat[i]);
     }
 
-    // Output averages
+    
     printf("\nAverage Waiting Time: %.2f", wavg);
     printf("\nAverage Turnaround Time: %.2f\n", tatavg);
 
